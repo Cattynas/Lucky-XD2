667a75023fe2b9f3b94c6fc29abb1707702d47de
Cattynas <stephensaviour779@gmail.com>
2025-07-27 18:40:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lucky~6FkTUDCB#uR7GrM_sLXHbI9U1AugV7OdRZxRycT8I5xJjAJ-oQEU",
+SESSION_ID: process.env.SESSION_ID || "lucky~XFMxwbpK#tPBBsWd_DMiuxVB0mr75dI6EYmYC5-ydYfp4n1tXrD4",
 // add your Session Id make sure it starts with lucky~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -68,7 +68,7 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "09049602172",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "+2349049602172",
 // add your bot owner number
 
 OWNER_NAME: process.env.OWNER_NAME || "Catty Nas",
